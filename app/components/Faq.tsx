b9a2206a441b7822774bdd7de4e669e59222911f
abdessadek <abdessadekbirama1@gmail.com>
2025-12-17 18:33:36 +0100

    fix faq

--- app/components/Faq.tsx
+++ app/components/Faq.tsx
@@ -1,8 +1,8 @@
-"use client";
-import SectionTitle from "./SectionTitle";
-import { data } from "../data/faq";
-import { useState } from "react";
-import { motion } from "framer-motion";
+// "use client";
+// import SectionTitle from "./SectionTitle";
+// import { data } from "../data/faq";
+// import { useEffect, useState } from "react";
+// import { motion } from "framer-motion";
 
 // export default function Faq() {
 //   const services = [
@@ -13,15 +13,52 @@ import { motion } from "framer-motion";
 //     "WKO-beheer",
 //     "Trainingen",
 //   ];
-//   const [active, setActive] = useState("Algemene vragen");
+//   const [active, setActive] = useState<string>("Algemene vragen");
+//   const [open, setOpen] = useState<number | null>(null);
 
 //   const toggleActive = ({ title }: { title: string }) => {
 //     setActive(title);
 //   };
 
+//   const containerVariants = {
+//     hidden: { opacity: 0 },
+//     visible: {
+//       opacity: 1,
+//       transition: {
+//         staggerChildren: 0.08,
+//         delayChildren: 0.1,
+//       },
+//     },
+//   } as const;
+
+//   const itemVariants = {
+//     hidden: {
+//       opacity: 0,
+//       y: 20,
+//       scale: 0.95,
+//     },
+//     visible: {
+//       opacity: 1,
+//       y: 0,
+//       scale: 1,
+//       transition: {
+//         duration: 0.4,
+//         ease: "easeOut",
+//       },
+//     },
+//   } as const;
+
+//   const toggleOpen = (index: any) => {
+//     setOpen(index);
+//   };
+
+//   useEffect(() => {
+//     setOpen(null);
+//   }, [active]);
+
 //   return (
 //     <div
-//       className="my-40 flex flex-col items-center"
+//       className="my-10 md:mt-18 flex flex-col items-center pt-10"
 //       style={{ fontFamily: "Poppins" }}
 //     >
 //       <SectionTitle
@@ -33,47 +70,120 @@ import { motion } from "framer-motion";
 //           </div>
 //         }
 //       />
-//       <div className="text-[#254055] flex gap-3 mt-13 max-w-3/4">
+//       <div className="text-[#254055] flex gap-3 md:mt-13 mt-10 md:max-w-3/4">
 //         {/* services */}
 //         <div className="flex flex-col gap-2">
 //           {data.map((faq, index) => (
-//             <button
-//               onClick={() => toggleActive({ title: faq.title })}
-//               key={index}
-//               className={`text-left font-semibold rounded-lg pl-5 pr-40 text-nowrap py-4 hover:cursor-pointer transition-colors duration-200 ${
-//                 active === faq.title
-//                   ? "bg-[#F0F8E2]"
-//                   : "bg-[#F6F6F6] hover:bg-[#f0f8e2c4]"
-//               }`}
-//             >
-//               {faq.title}
-//             </button>
+//             <div key={index} className="flex flex-col gap-2">
+//               <motion.button
+//                 onClick={() => toggleActive({ title: faq.title })}
+//                 key={index}
+//                 className={`md:mx-0 mx-10 text-left font-semibold rounded-lg pl-5 md:pr-40 text-nowrap py-4 hover:cursor-pointer transition-colors duration-200 ${
+//                   active === faq.title
+//                     ? "bg-[#F0F8E2]"
+//                     : "bg-[#F6F6F6] hover:bg-[#f0f8e2b5]"
+//                 }`}
+//               >
+//                 {faq.title}
+//               </motion.button>
+//               {active === faq.title ? (
+//                 <motion.div
+//                   key={active}
+//                   variants={containerVariants}
+//                   initial="hidden"
+//                   animate="visible"
+//                   className="md:hidden flex flex-col gap-2 mx-5 mt-1"
+//                 >
+//                   {data[services.indexOf(active)].faqs.map((faq, index) => (
+//                     <motion.div
+//                       key={index}
+//                       variants={itemVariants}
+//                       className="bg-[#F0F8E2] py-4 rounded-xl pl-2 relative"
+//                     >
+//                       <motion.div
+//                         className="bg-[#254055] size-3 rounded absolute"
+//                         initial={{ scale: 0 }}
+//                         animate={{ scale: 1 }}
+//                         transition={{
+//                           delay: 0.1 + index * 0.08,
+//                           duration: 0.3,
+//                         }}
+//                       />
+//                       <motion.div
+//                         className="bg-[#254055] w-2 h-0.5 rounded absolute right-3 mt-2"
+//                         initial={{ scaleX: 0 }}
+//                         animate={{ scaleX: 1 }}
+//                         transition={{
+//                           delay: 0.15 + index * 0.08,
+//                           duration: 0.3,
+//                         }}
+//                       />
+//                       <div className="">
+//                         <h1 className="text-[13px] mx-6">{faq.question}</h1>
+//                         <p className="text-[#808080] mt-2 text-xs mr-2">
+//                           {faq.answer}
+//                         </p>
+//                       </div>
+//                     </motion.div>
+//                   ))}
+//                 </motion.div>
+//               ) : null}
+//             </div>
 //           ))}
 //         </div>
 
 //         {/* FAQs */}
-//         <div className="flex flex-col gap-2">
+//         <motion.div
+//           key={active}
+//           variants={containerVariants}
+//           initial="visible"
+//           // animate="visible"
+//           className="md:flex hidden flex-col gap-2"
+//         >
 //           {data[services.indexOf(active)].faqs.map((faq, index) => (
 //             <motion.div
-//               initial={{ opacity: 0.5 }}
-//               animate={{ opacity: 1 }}
 //               key={index}
-//               transition={{ duration: 0.6 }}
-//               className="bg-[#F0F8E2] p-4 rounded-xl pl-16 relative"
+//               variants={itemVariants}
+//               className={`bg-[#F0F8E2] p-4 rounded-xl pl-16 relative`}
 //             >
-//               <div className="bg-[#254055] size-3 rounded absolute left-3"></div>
-//               <div className="bg-[#254055] w-3 h-0.5 rounded absolute right-5 mt-2"></div>
-//               <div className="pr-5">
+//               <motion.div
+//                 className="bg-[#254055] size-3 rounded absolute left-3 top-6"
+//                 initial={{ scale: 0 }}
+//                 animate={{ scale: 1 }}
+//                 transition={{ delay: 0.1 + index * 0.08, duration: 0.3 }}
+//               />
+//               <motion.button
+//                 onClick={() => toggleOpen(index)}
+//                 className="w-3 h-0.5 text-3xl rounded absolute right-5 hover:cursor-pointer p-3 flex justify-center items-center"
+//                 initial={{ scaleX: 0 }}
+//                 animate={{ scaleX: 1 }}
+//                 transition={{ delay: 0.15 + index * 0.08, duration: 0.3 }}
+//               >
+//                 {open === index ? "-" : "+"}
+//               </motion.button>
+//               <div className={`${open === index ? "pr-5" : "pr-20"}`}>
 //                 <h1>{faq.question}</h1>
-//                 <p className="text-[#808080] mt-2 text-[15px]">{faq.answer}</p>
+//                 <p
+//                   className={`text-[#808080] mt-2 text-[15px] ${
+//                     open === index ? "" : "hidden"
+//                   }`}
+//                 >
+//                   {faq.answer}
+//                 </p>
 //               </div>
 //             </motion.div>
 //           ))}
-//         </div>
+//         </motion.div>
 //       </div>
 //     </div>
 //   );
 // }
+
+"use client";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import SectionTitle from "./SectionTitle";
+import { data } from "../data/faq";
 
 export default function Faq() {
   const services = [
@@ -85,6 +195,7 @@ export default function Faq() {
     "Trainingen",
   ];
   const [active, setActive] = useState<string>("Algemene vragen");
+  const [open, setOpen] = useState<number | null>(null);
 
   const toggleActive = ({ title }: { title: string }) => {
     setActive(title);
@@ -117,6 +228,14 @@ export default function Faq() {
       },
     },
   } as const;
+
+  const toggleOpen = (index: number | null) => {
+    setOpen(index);
+  };
+
+  useEffect(() => {
+    setOpen(null);
+  }, [active]);
 
   return (
     <div
@@ -171,20 +290,45 @@ export default function Faq() {
                           duration: 0.3,
                         }}
                       />
-                      <motion.div
-                        className="bg-[#254055] w-3 h-0.5 rounded absolute right-2 mt-2"
+                      <motion.button
+                        onClick={() =>
+                          toggleOpen(open === index ? null : index)
+                        }
+                        className="w-2 h-0.5 rounded absolute right-3 text-xl flex justify-center items-center p-3"
                         initial={{ scaleX: 0 }}
                         animate={{ scaleX: 1 }}
                         transition={{
                           delay: 0.15 + index * 0.08,
                           duration: 0.3,
                         }}
-                      />
+                      >
+                        {open === index ? "-" : "+"}
+                      </motion.button>
                       <div className="">
-                        <h1 className="text-[13px] mx-6">{faq.question}</h1>
-                        <p className="text-[#808080] mt-2 text-xs">
-                          {faq.answer}
-                        </p>
+                        <h1
+                          className="text-[13px] mx-6 mr-10 hover:cursor-pointer"
+                          onClick={() =>
+                            toggleOpen(open === index ? null : index)
+                          }
+                        >
+                          {faq.question}
+                        </h1>
+                        <motion.div
+                          initial={false}
+                          animate={{
+                            height: open === index ? "auto" : 0,
+                            opacity: open === index ? 1 : 0,
+                          }}
+                          transition={{
+                            height: { duration: 0.3, ease: "easeInOut" },
+                            opacity: { duration: 0.2, ease: "easeInOut" },
+                          }}
+                          style={{ overflow: "hidden" }}
+                        >
+                          <p className="text-[#808080] mt-2 text-xs mr-2 mx-6">
+                            {faq.answer}
+                          </p>
+                        </motion.div>
                       </div>
                     </motion.div>
                   ))}
@@ -199,14 +343,13 @@ export default function Faq() {
           key={active}
           variants={containerVariants}
           initial="visible"
-          // animate="visible"
           className="md:flex hidden flex-col gap-2"
         >
           {data[services.indexOf(active)].faqs.map((faq, index) => (
             <motion.div
               key={index}
               variants={itemVariants}
-              className="bg-[#F0F8E2] p-4 rounded-xl pl-16 relative"
+              className={`bg-[#F0F8E2] p-4 rounded-xl pl-16 relative`}
             >
               <motion.div
                 className="bg-[#254055] size-3 rounded absolute left-3 top-6"
@@ -214,15 +357,38 @@ export default function Faq() {
                 animate={{ scale: 1 }}
                 transition={{ delay: 0.1 + index * 0.08, duration: 0.3 }}
               />
-              <motion.div
-                className="bg-[#254055] w-3 h-0.5 rounded absolute right-5 mt-2"
+              <motion.button
+                onClick={() => toggleOpen(open === index ? null : index)}
+                className="w-3 h-0.5 text-3xl rounded absolute right-5 hover:cursor-pointer p-3 flex justify-center items-center"
                 initial={{ scaleX: 0 }}
                 animate={{ scaleX: 1 }}
                 transition={{ delay: 0.15 + index * 0.08, duration: 0.3 }}
-              />
-              <div className="pr-5">
-                <h1>{faq.question}</h1>
-                <p className="text-[#808080] mt-2 text-[15px]">{faq.answer}</p>
+              >
+                {open === index ? "-" : "+"}
+              </motion.button>
+              <div className={`${open === index ? "pr-5" : "pr-20"}`}>
+                <h1
+                  className="hover:cursor-pointer"
+                  onClick={() => toggleOpen(open === index ? null : index)}
+                >
+                  {faq.question}
+                </h1>
+                <motion.div
+                  initial={false}
+                  animate={{
+                    height: open === index ? "auto" : 0,
+                    opacity: open === index ? 1 : 0,
+                  }}
+                  transition={{
+                    height: { duration: 0.3, ease: "easeInOut" },
+                    opacity: { duration: 0.2, ease: "easeInOut" },
+                  }}
+                  style={{ overflow: "hidden" }}
+                >
+                  <p className="text-[#808080] mt-2 text-[15px]">
+                    {faq.answer}
+                  </p>
+                </motion.div>
               </div>
             </motion.div>
           ))}
