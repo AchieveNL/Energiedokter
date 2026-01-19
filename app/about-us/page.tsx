0409fb5b831b029f66b7d8478a22577ab022e356
abdessadek <abdessadekbirama1@gmail.com>
2026-01-19 12:31:34 +0100

    fix issues

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -37,10 +37,10 @@ export default function AboutPage() {
       <div className="md:mt-32 mt-20"></div>
       {/* help */}
       <div
-        className="bg-[url('/assets/HomeHeroBg.svg')] bg-cover md:p-7 p-3 rounded-tl-4xl rounded-br-4xl md:mx-10 mx-3 relative"
+        className="bg-[url('/assets/HomeHeroBg.svg')] bg-cover lg:p-7 p-3 rounded-tl-4xl rounded-br-4xl md:mx-10 xl:mx-24 mx-3 relative"
         style={{ fontFamily: "Poppins" }}
       >
-        <div className="md:w-auto w-2/3 pb-3">
+        <div className="lg:w-auto w-2/3 pb-3">
           <h1 className="text-[#0D2131] font-bold md:text-2xl pb-2 ">
             Last van energieverlies? De{" "}
             <span className="text-[#81C713]">Energie Dokter</span> helpt!
@@ -51,7 +51,7 @@ export default function AboutPage() {
         </div>
         {/* links */}
 
-        <div className="md:flex hidden md:gap-5 gap-2 mt-5 items-center">
+        <div className="lg:flex hidden md:gap-5 gap-2 mt-5 items-center">
           <Link
             href="/contact"
             className="md:border-2 border border-[#81C713] bg-[#81C713] text-white md:text-base text-sm flex justify-center items-center h-fit rounded-xl transition-all md:px-5 p-2 px-3 text-nowrap hover:bg-[#5a8613] hover:border-[#5a8613]"
@@ -67,21 +67,21 @@ export default function AboutPage() {
         </div>
         <img
           src="/assets/about/man.svg"
-          className="absolute bottom-0 md:right-32 right-5 h-[110%] md:block hidden"
+          className="absolute bottom-0 md:right-32 right-5 h-[110%] lg:block hidden"
           alt=""
         />
         <img
           src="/assets/about/man.png"
-          className="absolute bottom-0 md:right-32 right-5 h-[110%] md:hidden block"
+          className="absolute bottom-0 lg:right-32 right-5 h-[110%] lg:hidden block"
           alt=""
         />
-        <div className="absolute right-6 md:top-2 bottom-0 md:text-[#254055] text-white text-center md:bg-white rounded-tl-2xl rounded-br-2xl h-fit px-3 md:py-3 ">
+        <div className="absolute right-6 lg:top-2 bottom-0 lg:text-[#254055] text-white text-center lg:bg-white rounded-tl-2xl rounded-br-2xl h-fit px-3 md:py-3 ">
           <h1 className="font-bold md:text-base text-xs ">Rutger Jenner</h1>
           <p className="md:text-[8px] text-[6px] ">Oprichter en Directeur</p>
         </div>
       </div>
       {/* mobile links */}
-      <div className="md:hidden justify-center flex md:gap-5 gap-2 mt-5 items-center">
+      <div className="lg:hidden justify-center flex md:gap-5 gap-2 mt-5 items-center">
         <Link
           href="/contact"
           className="md:border-2 border border-[#81C713] bg-[#81C713] text-white md:text-base text-sm flex justify-center items-center h-fit rounded-xl transition-all md:px-5 p-2 px-3 text-nowrap hover:bg-[#5a8613] hover:border-[#5a8613]"
