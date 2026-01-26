75502f95acc3d0107a0de6469759c8ec4826802d
abdessadek <abdessadekbirama1@gmail.com>
2026-01-26 22:31:39 +0100

    fix issues

--- app/components/StepPlan.tsx
+++ app/components/StepPlan.tsx
@@ -1,13 +1,77 @@
+// "use client";
+// import { useRef, useState } from "react";
+// import Plan from "./Plan";
+// import { planSteps } from "../data/planSteps";
+// import Lottie, { LottieRefCurrentProps } from "lottie-react";
+// import animationData from "@/public/assets/animations/turbine.json";
+
+// export default function StepPlan({
+//   activeBtn = "Onze aanpak",
+// }: {
+//   activeBtn?: string;
+// }) {
+//   const LottieRef = useRef<LottieRefCurrentProps>(null);
+
+//   const [active, setActive] = useState(activeBtn);
+
+//   const toggleActive = (e: React.MouseEvent<HTMLButtonElement>) => {
+//     setActive(e.currentTarget.innerText);
+//   };
+
+//   const NavLink = ({ text }: { text: string }) => (
+//     <button
+//       onClick={toggleActive}
+//       className={`md:text-[16px] text-xs font-semibold md:py-4 py-3 md:px-7 px-5 md:rounded-2xl rounded-xl hover:cursor-pointer transition-all duration-300 text-nowrap relative overflow-hidden ${
+//         active === text
+//           ? "bg-[#81C713] text-white"
+//           : "text-[#254055] bg-white hover:bg-[#82c71324]"
+//       }`}
+//     >
+//       <span className="relative z-10">{text}</span>
+//     </button>
+//   );
+
+//   return (
+//     <div
+//       className="relative overflow-hidden bg-[linear-gradient(to_top,#FFFFFF,#F2F2CA80,#E5F0BFBF,#D0F0E7BF)] md:py-10 py-5 px-5 min-[768px]:px-20 min-[1280px]:px-24 min-[1700px]:px-50 min-[2000px]:px-80"
+//       style={{ fontFamily: "Poppins" }}
+//     >
+//       <div className="absolute bottom-0 w-full inset-0 h-full">
+//         <Lottie
+//           lottieRef={LottieRef}
+//           animationData={animationData}
+//           loop
+//           autoplay
+//           style={{ width: "100%", height: "auto" }}
+//         />
+//       </div>
+//       <nav className="flex flex-wrap items-center xl:justify-center gap-2 md:gap-5">
+//         <NavLink text="Onze aanpak" />
+//         <NavLink text="Energielabels" />
+//         <NavLink text="GACS" />
+//         <NavLink text="Informatieplicht" />
+//         <NavLink text="WKO-beheer" />
+//         <NavLink text="Trainingen" />
+//       </nav>
+//       <Plan active={active} />
+//     </div>
+//   );
+// }
+
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Plan from "./Plan";
 import { planSteps } from "../data/planSteps";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
+import animationData from "@/public/assets/animations/turbine.json";
 
 export default function StepPlan({
   activeBtn = "Onze aanpak",
 }: {
   activeBtn?: string;
 }) {
+  const LottieRef = useRef<LottieRefCurrentProps>(null);
+
   const [active, setActive] = useState(activeBtn);
 
   const toggleActive = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -29,10 +93,19 @@ export default function StepPlan({
 
   return (
     <div
-      className="bg-[linear-gradient(to_top,#FFFFFF,#F2F2CA80,#E5F0BFBF,#D0F0E7BF)] md:py-10 py-5 px-5 min-[768px]:px-20 min-[1280px]:px-24 min-[1700px]:px-50 min-[2000px]:px-80"
+      className="relative overflow-hidden bg-[linear-gradient(to_top,#FFFFFF,#F2F2CA80,#E5F0BFBF,#D0F0E7BF)] md:py-10 py-5 px-5 min-[768px]:px-20 min-[1280px]:px-24 min-[1700px]:px-50 min-[2000px]:px-80"
       style={{ fontFamily: "Poppins" }}
     >
-      <nav className="flex flex-wrap items-center xl:justify-center gap-2 md:gap-5">
+      <div className="absolute bottom-0 left-0 right-0 w-full flex items-end">
+        <Lottie
+          lottieRef={LottieRef}
+          animationData={animationData}
+          loop
+          autoplay
+          style={{ width: "100%", height: "auto" }}
+        />
+      </div>
+      <nav className="flex flex-wrap items-center xl:justify-center gap-2 md:gap-5 relative z-10">
         <NavLink text="Onze aanpak" />
         <NavLink text="Energielabels" />
         <NavLink text="GACS" />
